285f7b3d05705924a99b9b7f330c711a809a41bb
lalitladumor280697 <lalitladumor.280697@gmail.com>
2025-05-22 17:38:39 +0530

    bb

---
+++ app.py
@@ -0,0 +1,124 @@
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+import collections
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/input-board", methods=['POST'])
+def input_board():
+    suduko = []
+    row1 = request.form.get('row1',type=list)
+    row2 = request.form.get('row2',type=list)
+    row3 = request.form.get('row3',type=list)
+    row4 = request.form.get('row4',type=list)
+    row5 = request.form.get('row5',type=list)
+    row6 = request.form.get('row6',type=list)
+    row7 = request.form.get('row7',type=list)
+    row8 = request.form.get('row8',type=list)
+    row9 = request.form.get('row9',type=list)
+    suduko.append(row1)
+    suduko.append(row2)
+    suduko.append(row3)
+    suduko.append(row4)
+    suduko.append(row5)
+    suduko.append(row6)
+    suduko.append(row7)
+    suduko.append(row8)
+    suduko.append(row9)
+    return jsonify({"board": suduko})
+
+# check if sudoku is valid or not
+@app.route("/isvalid", methods=['POST'])
+def isvalid():
+    board = request.get_json()
+    board = board['board']
+
+    cols = collections.defaultdict(set)
+    rows = collections.defaultdict(set)
+    squares = collections.defaultdict(set)  # key = (r /3, c /3)
+
+    for r in range(9):
+        for c in range(9):
+            # skip if it's empty cell
+            if board[r][c] == ".":
+                continue
+                
+            # if element is present in row, col or square then return false, it's not valid suduko.
+            if (board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r // 3, c // 3)]):
+                return jsonify({"valid": False})
+            
+            # otherwise add it into row, col, and square
+            cols[c].add(board[r][c])
+            rows[r].add(board[r][c])
+            squares[(r // 3, c // 3)].add(board[r][c])
+
+    # valid suduko
+    return jsonify({"valid": True})
+
+# solve sudoku
+@app.route("/solve", methods=['POST'])
+def solveSudoku():
+    board = request.get_json()
+    board = board['board']
+
+    helper(board, 0, 0)
+    return jsonify({"solution": board})
+
+def helper(board, row, col):
+    if row == len(board):
+        return True
+        
+    nrow = 0
+    ncol = 0
+    
+    if col == len(board)-1:
+        nrow = row + 1
+        ncol = 0
+    
+    else:
+        nrow = row
+        ncol = col + 1
+    
+    if board[row][col] != '.':
+        if helper(board, nrow, ncol):
+            return True
+    else:
+        # fill the place
+        for i in range(1, 10):
+            if isSafe(board, row, col, i):
+                board[row][col] = str(i)
+                if helper(board, nrow, ncol):
+                    return True
+                else:
+                        board[row][col] = '.'
+    return False
+
+def isSafe(board, row, col, number):
+    # column
+    for i in range(len(board)):
+        if board[i][col] == str(number):
+            return False
+
+    # row
+    for j in range(len(board)):
+        if board[row][j] == str(number):
+            return False
+
+    # grid
+    sr = 3 * (row//3)
+    sc = 3 * (col//3)
+
+    for i in range(sr, sr+3):
+        for j in range(sc, sc+3):
+            if board[i][j] == str(number):
+                return False
+    
+    return True
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,31 @@
+- We are going to create web app to solve any sudoku (easy to hard level)
+<img width="338" alt="image" src="https://user-images.githubusercontent.com/46566839/194050368-84c97c35-c807-4f9d-864b-a0885a125db1.png">
+
+
+## Getting started
+* Register yourself in the [Hacktoberfest](https://hacktoberfest.com/) 2022 
+* Fork this repository (Click the Fork button in the top right of this page, click your Profile Image)
+* Clone your fork down to your local machine
+
+```markdown
+git clone https://github.com/dhirajberi/sudoko-solver.git
+``` 
+
+* Commit and push
+
+```markdown
+git add .
+git commit -m 'Commit message'
+git push origin branch-name
+```
+
+* Create a new pull request from your forked repository (Click the `New Pull Request` button located at the top of your repo)
+* Wait for your PR review and merge approval!
+* __Star this repository__ if you had fun!
+
+## 👩‍💻👨‍💻 Our valuable Contributors
+
+<p align="center">
+<a href="https://github.com/dhirajberi/sudoku-solver/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=dhirajberi/sudoku-solver" />
+</a></p>
